3a65de8f1af69ee78e118090023df6dade3be749
wuyu1001 <wuyuaginst@gmail.com>
2017-10-23 00:08:53 +0800

    add psql

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
+    
 
 
     # Put any configurations here that are common across all environments
@@ -9,6 +14,8 @@ class Config(object):
 class ProductionConfig(Config):
 
     DEBUG = False
+    SECRET_KEY = 'p9Bv<3Eid9%$i01' 
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class DevelopmentConfig(Config):
     DEBUG = True
--- run.py
+++ run.py
@@ -2,7 +2,6 @@ import os
 
 from myapp import create_app
 
-basedir = os.path.abspath(os.path.dirname(__file__))
 
 config_name = os.getenv('FLASK_CONFIG')
 app = create_app(config_name)
